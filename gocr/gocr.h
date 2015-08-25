e1f7a7126cbebb8f354090809618d3f2933d0164
Carl Wieland <carl@chiefarchitect.com>
2015-08-25 12:00:47 -0700

    Initial Commit

---
+++ gocr/gocr.h
@@ -0,0 +1,19 @@
+//
+//  gocr.h
+//  gocr
+//
+//  Created by Carl Wieland on 8/25/15.
+//  Copyright © 2015 Carl Wieland. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for gocr.
+FOUNDATION_EXPORT double gocrVersionNumber;
+
+//! Project version string for gocr.
+FOUNDATION_EXPORT const unsigned char gocrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <gocr/PublicHeader.h>
+
+
